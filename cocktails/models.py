10c2b31b4c14512e47753a57314412fbe88b22b0
nmuldavin <nmuldavin@gmail.com>
2016-02-24 10:18:45 -0800

    Set up cocktail model

--- cocktails/models.py
+++ cocktails/models.py
@@ -1,5 +1,21 @@
 from __future__ import unicode_literals
 
+from django.contrib import admin
+from django.template.defaultfilters import slugify
 from django.db import models
 
-# Create your models here.
+class Cocktail(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(default='', blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+                self.slug = slugify(self.name)
+                super(Cocktail, self).save(*args, **kwargs)
+
+class CocktailAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+
+admin.site.register(Cocktail, CocktailAdmin)
