7b24bd22bfd9726aa5172c2f5224e5022bd5cf5e
nmuldavin <nmuldavin@gmail.com>
2016-02-24 19:54:17 -0800

    Starting Recipes

--- cocktails/admin.py
+++ cocktails/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Ingredient, Recipe, Cocktail, PurchasedIngredient
+from .models import Ingredient, Recipe, Cocktail, PurchasedIngredient, HomemadeIngredient
 
 
 
@@ -8,9 +8,10 @@ class IngredientAdmin(admin.ModelAdmin):
 
 admin.site.register(Ingredient, IngredientAdmin)
 admin.site.register(PurchasedIngredient)
+admin.site.register(HomemadeIngredient)
 
 class RecipeAdmin(admin.ModelAdmin):
-    prepopulated_fields = {"slug": ("name",)}
+    prepopulated_fields = {"slug": ("descriptor",)}
 
 admin.site.register(Recipe, RecipeAdmin)
 
