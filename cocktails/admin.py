03d6d468ae2d082d21f7646b4a98f75beb0f1eb4
nmuldavin <nmuldavin@gmail.com>
2016-03-02 10:03:42 -0800

    Set up ingredient model and admin interface

--- cocktails/admin.py
+++ cocktails/admin.py
@@ -1,21 +1,7 @@
 from django.contrib import admin
-from .models import Ingredient, Recipe, Cocktail, PurchasedIngredient, HomemadeIngredient
-
-
+from .models import Ingredient
 
 class IngredientAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
 
 admin.site.register(Ingredient, IngredientAdmin)
-admin.site.register(PurchasedIngredient)
-admin.site.register(HomemadeIngredient)
-
-class RecipeAdmin(admin.ModelAdmin):
-    prepopulated_fields = {"slug": ("descriptor",)}
-
-admin.site.register(Recipe, RecipeAdmin)
-
-class CocktailAdmin(admin.ModelAdmin):
-    prepopulated_fields = {"slug": ("name",)}
-
-admin.site.register(Cocktail, CocktailAdmin)
