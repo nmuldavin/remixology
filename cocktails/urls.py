9075de20850041dcd5ee886ed117f0e320933591
nmuldavin <nmuldavin@gmail.com>
2016-03-02 21:25:44 -0800

    Set up Ingredient Template

--- cocktails/urls.py
+++ cocktails/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name='cocktails'
 
 urlpatterns = [
-    url(r'^ingredients/', views.IngredientView.as_view(), name='ingredients')
+    url(r'^ingredients/(?P<ingredient_slug>[-\w]+)', views.IngredientView.as_view(), name='ingredients')
 ]
--- cocktails/views.py
+++ cocktails/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import render
 from django.views.generic import View
+from .models import Ingredient
 
 class IngredientView(View):
 
-    def get(self, request):
 
-        return HttpResponse('It worked')
+
+    def get(self, request, *args, **kwargs):
+
+        ctx = {}
+
+        ingredient_slug = self.kwargs['ingredient_slug']
+
+        try:
+            ingredient = Ingredient.objects.get(slug=ingredient_slug)
+            ctx['ingredient'] = ingredient
+        except:
+            pass
+
+        return render(request, 'cocktails/ingredient.html', ctx)
+
