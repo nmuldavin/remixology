97b6b01ff40068d9a5fe099466332bee79498a2d
nmuldavin <nmuldavin@gmail.com>
2016-03-06 11:24:12 -0800

    Saved Work

--- cocktails/static/cocktails/js/ajax.js
+++ cocktails/static/cocktails/js/ajax.js
@@ -2,23 +2,45 @@ $(document).ready(function() {
 
     var slug = $("#recipe_container").attr("data-group");
     var rank = parseInt($("#recipe_container").attr("data-rank"));
+    var current = [];
+    var standard = [];
 
-    function loadrecipe(groupslug, reciperank) {
-        $("#recipe_container").html('').load(
-            "/cocktails/"+ groupslug + "/" + String(reciperank)
-        );
+    function getRecipe() {
+        var recipe = [];
+        $('.recipe-entry').each(function() {
+            var entry = []
+            $(this).children('.entry-data').each(function() {
+                entry.push($(this).html());
+            });
+            recipe.push(entry);
+        });
+        return recipe;
     };
 
-    loadrecipe(slug, rank);
+    function loadrecipe(groupslug, reciperank, init) {
+        $("#recipe_container").html('').load(
+            "/cocktails/"+ groupslug + "/" + String(reciperank), function() {
+                current = getRecipe();
+                if(init) {
+                    standard = current;
+                    console.log(standard);
+                }
+                console.log(current);
+            });
+    };
+
+
+
+    loadrecipe(slug, rank, true);
 
     $("#next").click(function() {
         rank = rank + 1;
-        loadrecipe(slug, rank);
+        loadrecipe(slug, rank, false);
     });
 
     $("#previous").click(function() {
         rank = rank - 1;
-        loadrecipe(slug, rank);
+        loadrecipe(slug, rank, false);
 
     });
 });
