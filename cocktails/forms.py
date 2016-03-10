f1cca16c2fbd835362898617cd9d20856ac07f13
nmuldavin <nmuldavin@gmail.com>
2016-03-09 16:13:48 -0800

    Setting up Cocktail Template

--- cocktails/forms.py
+++ cocktails/forms.py
@@ -7,4 +7,9 @@ class CocktailForm (forms.ModelForm):
     class Meta:
         model = Cocktail
         exclude = ('slug', 'type')
+        widgets = {
+            'description' : forms.Textarea(attrs={
+                'placeholder' : 'Description'
+            }),
+        }
 
