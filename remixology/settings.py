9420da41374194a96ed9fd1f953634f2cb1c1732
nmuldavin <nmuldavin@gmail.com>
2016-03-24 17:57:02 -0700

    Saving Work

--- remixology/settings.py
+++ remixology/settings.py
@@ -66,16 +66,16 @@ TEMPLATES = [
 #Database
 #https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-# urlparse.uses_netloc.append("postgres")
-# url = urlparse.urlparse(os.environ["DATABASE_URL"])
-#
-# conn = psycopg2.connect(
-#     database=url.path[1:],
-#     user=url.username,
-#     password=url.password,
-#     host=url.hostname,
-#     port=url.port
-# )
+urlparse.uses_netloc.append("postgres")
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
+
+conn = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+)
 
 
 
